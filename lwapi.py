04a4a149ad4eec29b85f8e39fbc5a93c8cccd92d
Fred <stevec@liquidweb.com>
2013-08-23 20:23:59 -0400

    Added Files from the project as it currently stands.  lwapi.py - contains the LWApi class, used to make requests to the storm api  lwexceptions.py - contains exceptions that might be thrown in the course of doing so

---
+++ lwapi.py
@@ -0,0 +1,164 @@
+"""stormpy - a simple client library for accessing the Liquid Web Storm API, written in python
+"""
+
+import os
+import requests
+import urllib
+import time
+import json
+from getpass import getpass
+from lwexceptions import *
+
+class LWApi(object):
+  """
+    user - the api user (a string)
+    password - the user's password (a string). If the password is omitted, it will need to be entered via stdin anytime the auth token needs to be updated. This is only recommended for CLI applications. If CLI interactivity is not required, the password should be supplied.
+    api_version - the version of the api that will be used (a string). defaults to 'v1'. 'bleed' may also be used at the moment.
+    verify - whether the SSL certificate for the api should be verified (a bool). Defaults to True. This is primarily for testing. The public api should *always* have a valid SSL certificate!
+    docfile - name of the file that contains the api documentation in json format. This file may be downloaded here:
+      http://www.liquidweb.com/StormServers/api/docs/v1/docs.json
+    if no filename is supplied, the documentation will be downloaded automatically.
+  """
+  def __init__(self, user, password=None, api_version='v1', verify=True, docfile=None):
+    self._url = 'https://api.stormondemand.com/%s/' % api_version 
+    self._user = user
+    self._password = password
+
+    self._verify = verify
+
+    # auth token & expiry time. These will be set via calls to _get_token().
+    self._token = ''
+    self._expires = 0
+
+    # generate the dict of methods, based on json documentation.
+    if docfile:
+      docs = open(docfile,'r')
+    else:
+      docs = urllib.urlopen(('http://www.liquidweb.com/StormServers/api/docs/%s/docs.json' % api_version))
+
+    # we process the raw docs to make them a little easier to work with,
+    # so that each method is at the top level in the generated dict 
+    self._docs = {} 
+    j_docs = json.load(docs)
+
+    for path in j_docs.keys():
+      methods = j_docs[path]
+      for m in methods['__methods']:
+        method = path + '/' + m
+        self._docs[method] = methods['__methods'][m]
+
+    docs.close()
+
+  def _get_token(self):
+    # if no password is given, attempt to retreive it from stdin    
+    if self._password == None:
+      print "need to retrieve auth token. please enter password for user `%s`" % self._user
+      passwd = getpass('pass > ')
+    else:
+      passwd = self._password
+
+    auth = requests.auth.HTTPBasicAuth(self._user,passwd)
+    url = self._url + 'Account/Auth/token'
+    data = '{"params":{"timeout":"3600"}}'
+
+    r = requests.post(url=url, auth=auth, verify=self._verify)
+
+    if r.status_code != 200:
+      raise BadResponseException(r.status_code, r.text)
+
+    else:
+      self._token = json.loads(r.text)['token']
+      self._expires =  int(json.loads(r.text)['expires'])
+
+  def _get_auth(self):
+    now = int(time.time())
+    # if the auth token is not set, or if it has expired/is about to expire
+    if self._expires == 0 or now > self._expires + 5:
+      self._get_token()
+
+    return requests.auth.HTTPBasicAuth(self._user, self._token)
+      
+
+  def req(self, path, data={}):
+    """make a POST request to the storm api
+      path -- a string contaning the method you'd like to call. Methods are Case sensitive. Leading slash may be included or omitted
+        ex. 'Utilities/Info/ping'
+      
+      data -- POST data to be used by the request. 
+
+      returns a dictionary containing the server's response. This dictionary will be formatted according to the documentation:
+    """
+    # if the path starts with a /, strip it off.
+    if path[0] == '/':
+      path = path[1:]
+
+    # if the postdata is enclosed in a 'params' hash, just use the value.
+    # support the {"params":{"key":"value"}} because it complies with the
+    # docs, but the data is easier to use without it, so we'll put it in
+    # when we make the request.
+    if data.keys() == ['params']:
+      data = data['params']
+
+
+    # this is where input validation should take place
+    # presently, we just make sure that the required parameters are given
+    # but eventually we will add type checking
+
+    if path not in self._docs:
+      # invalid method!
+      raise NoMethodException(path) 
+
+    method = self._docs[path]
+
+    # check given parameters 
+    for param in data:
+      # make sure there aren't any we're not expecting 
+      if param not in method['__input']:
+        raise InvalidParamException(param, path)
+
+      # make sure the type is correct... but do that later
+      # ( type validation goes here )
+
+    # check expected parameters to make sure required/non-optional params are
+    # present... I guess there's a difference according to the docs? hoefully
+    # this will be addressed at some point, some input parameters are marked
+    # as optional: 1|0, others are marked as required as required: 1|0, but
+    # there seems to be no rhyme or reason to this.
+    for param in method['__input']:
+      param_dict = method['__input'][param]
+      if param not in data:
+        # if a parameter wasn't supplied and there's a default, use the default
+        if 'default' in param_dict:
+          default = param_dict['default']
+          data[param] = default
+          
+        # if the parameter is required/non-optional/whatever, and it's not
+        # present, throw an error
+        elif ('optional' in param_dict and param_dict['optional'] == '0')\
+        or ('required' in param_dict and param_dict['required'] == '1')\
+        or ('optional' not in param_dict and 'required' not in param_dict):
+          raise RequiredParamMissingException(param, path)
+
+    url = self._url + path
+    req = requests.post(url=url, auth=self._get_auth(),\
+                        data=json.dumps({'params':data}), verify=self._verify)
+
+    # make sure the request was completed successfully
+    if req.status_code != 200:
+      raise BadResponseException(req.status_code, req.text)
+
+    # turn the response into a json object
+    response = json.loads(req.text)
+
+    # handling errors: per the API docs, check the response for an 'error_class' key:
+    if 'error_class' in response:
+      ec = response['error_class']
+      if ec == 'LW::Exception::API::InvalidMethod':
+        # this error should be checked at input validation. getting this means docs are out of date!
+        raise Exception(response['full_message']) 
+      # unhandled errors...
+      else:
+        raise Exception(response)
+    
+    # no error:
+    return response
